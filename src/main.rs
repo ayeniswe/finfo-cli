6c302d88e19b85009ba8ed862f97ce3e58ec6daf
Samson <nazaritee@pm.pme>
2024-04-26 20:32:07 -0500

    feat: added print statement OHLC and traits

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-mod price;
-
-fn main() {}
+pub mod price;
+fn main() {
+    // let bar: price::Price = price::Price::new(4.03, 10.0, 1.0, 6.0);
+    // bar.show_ohlc(2);
+    // bar.show_traits()
+}
--- src/price.rs
+++ src/price.rs
@@ -1,26 +1,70 @@
 /// Open-High-Low-Close (OHLC)
-struct Price {
-    open: f32,
-    high: f32,
-    low: f32,
-    close: f32,
+#[derive(Default)]
+pub struct Price {
+    pub open: f32,
+    pub high: f32,
+    pub low: f32,
+    pub close: f32,
 }
 
 /// Popular trends in price
 ///
+/// TODO: may contain array of prices or current price
+///
+///
 /// `Custom` field allows for
 /// endless trends to be created based on
 /// needs
-struct Trends {
-    hourly: Price,
-    daily: Price,
-    weekly: Price,
-    monthly: Price,
-    yearly: Price,
-    custom: Price,
+pub struct Trends {
+    pub hourly: Price,
+    pub daily: Price,
+    pub weekly: Price,
+    pub monthly: Price,
+    pub yearly: Price,
+    pub custom: Price,
 }
 
 impl Price {
+    pub fn new(open: f32, high: f32, low: f32, close: f32) -> Self {
+        return Self {
+            open,
+            high,
+            low,
+            close,
+        };
+    }
+
+    /// Print the OHLC
+    ///
+    /// To prevent losing precision specfiy
+    /// the level of `precision` after decimal needed
+    ///
+    pub fn show_ohlc(&self, precision: usize) {
+        print!(" [Open] - {1:.0$}", precision, self.open);
+        print!("  [High] - {1:.0$}", precision, self.high);
+        print!("  [Low] - {1:.0$}", precision, self.low);
+        println!("  [Close] - {1:.0$}", precision, self.close);
+        println!("-------------------------------------------------------------");
+    }
+
+    /// Print the direction and strength of a candle
+    pub fn show_traits(&self) {
+        let dir = self.to_direction();
+        if dir == 'U' {
+            println!("Direction: ⬆️");
+        } else if dir == 'D' {
+            println!("Direction: ⬇️");
+        } else {
+            println!("Direction: ⬅️➡️");
+        }
+        if self.is_strong(None) {
+            println!("Strong: 💪");
+        } else {
+            println!("Strong: ❌");
+        }
+        println!("");
+    }
+
     /// Returns the percentage of movement in price
     ///
     /// # Examples
@@ -34,7 +78,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_bot_wick(), -0.8)
     /// ```
-    fn to_percent(&self) -> f32 {
+    pub fn to_percent(&self) -> f32 {
         (self.close - self.open) / self.open
     }
     /// Returns the raw point of movement in price
@@ -50,9 +94,10 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_point(), -4.0)
     /// ```
-    fn to_point(&self) -> f32 {
+    pub fn to_point(&self) -> f32 {
         self.close - self.open
     }
+
     /// Returns the total range of movement in price
     ///
     /// # Examples
@@ -66,9 +111,10 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_range(), 12.5)
     /// ```
-    fn to_range(&self) -> f32 {
+    pub fn to_range(&self) -> f32 {
         self.high - self.low
     }
+
     /// Returns the direction of price movement
     ///
     /// Movement can be one of the following:
@@ -90,7 +136,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_direction(), 'D')
     /// ```
-    fn to_direction(&self) -> char {
+    pub fn to_direction(&self) -> char {
         if self.close > self.open {
             return 'U';
         } else if self.close < self.open {
@@ -99,6 +145,7 @@ impl Price {
             return 'N';
         }
     }
+
     /// Returns the range of the top wick of a candle bar
     ///
     /// # Examples
@@ -112,7 +159,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_top_wick(), 8.0)
     /// ```
-    fn to_top_wick(&self) -> f32 {
+    pub fn to_top_wick(&self) -> f32 {
         let dir: char = self.to_direction();
         if dir == 'D' {
             return self.high - self.open;
@@ -120,6 +167,7 @@ impl Price {
             return self.high - self.close;
         }
     }
+
     /// Returns the range of the bottom wick of a candle bar
     ///
     /// # Examples
@@ -133,7 +181,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.to_bot_wick(), 0.5)
     /// ```
-    fn to_bot_wick(&self) -> f32 {
+    pub fn to_bot_wick(&self) -> f32 {
         let dir: char = self.to_direction();
         if dir == 'D' {
             return self.close - self.low;
@@ -141,6 +189,7 @@ impl Price {
             return self.open - self.low;
         }
     }
+
     /// Returns `true` if the candle is relatively weak based on
     /// the gauge level otherwise `false`
     ///
@@ -160,7 +209,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.is_weak(), false)
     /// ```
-    fn is_weak(&self, gauge: Option<f32>) -> bool {
+    pub fn is_weak(&self, gauge: Option<f32>) -> bool {
         let gauge: f32 = gauge.or(Some(1.0)).unwrap();
         let body: f32 = self.close - self.open;
         if body == 0.0 {
@@ -171,6 +220,7 @@ impl Price {
             return self.to_bot_wick() / body < -gauge;
         };
     }
+
     /// Returns `true` if the candle is relatively strong based on
     /// the gauge level otherwise `false`
     ///
@@ -190,7 +240,7 @@ impl Price {
     ///  };
     /// assert!eq(bar.is_strong(), true)
     /// ```
-    fn is_strong(&self, gauge: Option<f32>) -> bool {
+    pub fn is_strong(&self, gauge: Option<f32>) -> bool {
         let gauge: f32 = gauge.or(Some(2.0)).unwrap();
         let body: f32 = self.close - self.open;
         if body == 0.0 {
@@ -204,131 +254,136 @@ impl Price {
 }
 
 //////// PRICE //////////
-#[test]
-fn test_is_weak() {
-    let bar1: Price = Price {
-        open: 7.0,
-        close: 5.0,
-        low: 2.5,
-        high: 13.0,
-    };
-    let bar2: Price = Price {
-        open: 1.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    let bar3: Price = Price {
-        open: 5.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    assert_eq!(bar1.is_weak(None), true);
-    assert_eq!(bar2.is_weak(Some(1.0)), false);
-    assert_eq!(bar3.is_weak(None), false);
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_is_strong() {
-    let bar1: Price = Price {
-        open: 5.0,
-        close: 1.0,
-        low: 0.5,
-        high: 13.0,
-    };
-    let bar2: Price = Price {
-        open: 1.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    let bar3: Price = Price {
-        open: 5.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    assert_eq!(bar1.is_strong(None), true);
-    assert_eq!(bar2.is_strong(Some(3.0)), true);
-    assert_eq!(bar3.is_strong(None), false);
-}
+    #[test]
+    fn test_is_weak() {
+        let bar1: Price = Price {
+            open: 7.0,
+            close: 5.0,
+            low: 2.5,
+            high: 13.0,
+        };
+        let bar2: Price = Price {
+            open: 1.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        let bar3: Price = Price {
+            open: 5.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        assert_eq!(bar1.is_weak(None), true);
+        assert_eq!(bar2.is_weak(Some(1.0)), false);
+        assert_eq!(bar3.is_weak(None), false);
+    }
 
-#[test]
-fn test_to_direction() {
-    let bar1: Price = Price {
-        open: 5.0,
-        close: 1.0,
-        low: 0.5,
-        high: 13.0,
-    };
-    let bar2: Price = Price {
-        open: 1.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    let bar3: Price = Price {
-        open: 5.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    assert_eq!(bar1.to_direction(), 'D');
-    assert_eq!(bar2.to_direction(), 'U');
-    assert_eq!(bar3.to_direction(), 'N');
-}
+    #[test]
+    fn test_is_strong() {
+        let bar1: Price = Price {
+            open: 5.0,
+            close: 1.0,
+            low: 0.5,
+            high: 13.0,
+        };
+        let bar2: Price = Price {
+            open: 1.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        let bar3: Price = Price {
+            open: 5.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        assert_eq!(bar1.is_strong(None), true);
+        assert_eq!(bar2.is_strong(Some(3.0)), true);
+        assert_eq!(bar3.is_strong(None), false);
+    }
 
-#[test]
-fn test_to_range() {
-    let bar1: Price = Price {
-        open: 5.0,
-        close: 1.0,
-        low: 0.5,
-        high: 13.0,
-    };
-    assert_eq!(bar1.to_range(), 12.5);
-}
+    #[test]
+    fn test_to_direction() {
+        let bar1: Price = Price {
+            open: 5.0,
+            close: 1.0,
+            low: 0.5,
+            high: 13.0,
+        };
+        let bar2: Price = Price {
+            open: 1.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        let bar3: Price = Price {
+            open: 5.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        assert_eq!(bar1.to_direction(), 'D');
+        assert_eq!(bar2.to_direction(), 'U');
+        assert_eq!(bar3.to_direction(), 'N');
+    }
 
-#[test]
-fn test_to_point() {
-    let bar1: Price = Price {
-        open: 5.0,
-        close: 1.0,
-        low: 0.5,
-        high: 13.0,
-    };
-    let bar2: Price = Price {
-        open: 1.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    assert_eq!(bar1.to_point(), -4.0);
-    assert_eq!(bar2.to_point(), 4.0);
-}
+    #[test]
+    fn test_to_range() {
+        let bar1: Price = Price {
+            open: 5.0,
+            close: 1.0,
+            low: 0.5,
+            high: 13.0,
+        };
+        assert_eq!(bar1.to_range(), 12.5);
+    }
 
-#[test]
-fn test_to_percent() {
-    let bar1: Price = Price {
-        open: 5.0,
-        close: 1.0,
-        low: 0.5,
-        high: 13.0,
-    };
-    let bar2: Price = Price {
-        open: 1.0,
-        close: 5.0,
-        low: 5.5,
-        high: 6.0,
-    };
-    let bar3: Price = Price {
-        open: 1.0,
-        close: 1.0,
-        low: 0.6,
-        high: 1.5454,
-    };
-    assert_eq!(bar1.to_percent(), -0.8);
-    assert_eq!(bar2.to_percent(), 4.0);
-    assert_eq!(bar3.to_percent(), 0.0);
+    #[test]
+    fn test_to_point() {
+        let bar1: Price = Price {
+            open: 5.0,
+            close: 1.0,
+            low: 0.5,
+            high: 13.0,
+        };
+        let bar2: Price = Price {
+            open: 1.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        assert_eq!(bar1.to_point(), -4.0);
+        assert_eq!(bar2.to_point(), 4.0);
+    }
+
+    #[test]
+    fn test_to_percent() {
+        let bar1: Price = Price {
+            open: 5.0,
+            close: 1.0,
+            low: 0.5,
+            high: 13.0,
+        };
+        let bar2: Price = Price {
+            open: 1.0,
+            close: 5.0,
+            low: 5.5,
+            high: 6.0,
+        };
+        let bar3: Price = Price {
+            open: 1.0,
+            close: 1.0,
+            low: 0.6,
+            high: 1.5454,
+        };
+        assert_eq!(bar1.to_percent(), -0.8);
+        assert_eq!(bar2.to_percent(), 4.0);
+        assert_eq!(bar3.to_percent(), 0.0);
+    }
 }
