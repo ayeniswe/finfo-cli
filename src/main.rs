a14375d42a034ae84e15536158de5e7337bf7b8d
Ayeni <n5099f@us.lmco.com>
2024-04-30 16:56:49 -0600

    chore: clear out main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::io::stdout;
-
 pub mod color;
 pub mod price;
+
 fn main() {
-    let bar: price::Price = price::Price::new(4.03, 10.0, 1.0, 6.0);
-    bar.show_ohlc(2);
-    bar.show_direction(stdout().lock());
-    bar.show_strong(stdout().lock());
+    /// TODO
 }
