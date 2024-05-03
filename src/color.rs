ec97a8035654e1f7de7b9837e4e1ef0138acc4a5
Ayeni <n5099f@us.lmco.com>
2024-05-03 12:48:08 -0600

    feat: added another color and made orginal colors brighter

--- src/color.rs
+++ src/color.rs
@@ -1,4 +1,5 @@
 pub const RESET: &str = "\x1b[0m";
-pub const RED: &str = "\x1b[31m";
-pub const GREEN: &str = "\x1b[32m";
-pub const YELLOW: &str = "\x1b[33m";
+pub const RED: &str = "\x1b[38;2;255;0;0m";
+pub const GREEN: &str = "\x1b[38;2;0;255;0m";
+pub const YELLOW: &str = "\x1b[38;2;255;255;0m";
+pub const FAINT: &str = "\x1b[2m";
