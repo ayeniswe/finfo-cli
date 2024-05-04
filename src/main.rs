a02539ff79e5c244d3a735113747cceefa787673
Samson <nazaritee@pm.pme>
2024-05-04 18:24:15 -0500

    feat: added new command to list and added options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    io::{stdout, Write},
+    process,
+};
+
 pub mod color;
 pub mod price;
 
@@ -29,7 +34,23 @@ fn finfo_text_art() {
     );
 }
 
-fn main_menu(cmd_name: &str) {
+fn show_usage(cmd_name: &str) {
+    println!(
+        "Usage: {}{} [OPTIONS]... [COMMAND] TICKER{}",
+        color::FAINT,
+        cmd_name,
+        color::RESET
+    );
+    println!("Try '{} --help' for more information.", cmd_name);
+}
+
+fn log<T: Write>(mut output: T, msg: &str, verbose: bool) {
+    if verbose {
+        let _ = writeln!(output, "{}", msg);
+    }
+}
+
+fn show_help(cmd_name: &str) {
     // Title
     println!();
     finfo_text_art();
@@ -40,17 +61,12 @@ fn main_menu(cmd_name: &str) {
     println!();
     // Usage list
     println!(
-        "Usage: {}{} [OPTIONS]... TICKER [COMMAND]{}",
+        "Usage: {}{} [OPTIONS]... [COMMAND] TICKER{}",
         color::FAINT,
         cmd_name,
         color::RESET
     );
-    println!(
-        "Example: {}{} -N 'appl'{}",
-        color::FAINT,
-        cmd_name,
-        color::RESET
-    );
+    println!("Example: {} -n 'appl'", cmd_name);
     println!();
     // Options list
     println!("Options:");
@@ -61,37 +77,129 @@ fn main_menu(cmd_name: &str) {
     );
     println!("\t{}-v, --verbose{}\t\tVerbose", color::FAINT, color::RESET);
     println!(
-        "\t{}-q, --quiet{}\t\t\tHide log messages",
+        "\t{}-q, --quiet{}\t\tHide log messages",
         color::FAINT,
         color::RESET
     );
     println!(
-        "\t{}-o --offline{}\t\tRun without network requests",
+        "\t{}-o, --offline{}\t\tRun without network requests",
         color::FAINT,
         color::RESET
     );
-    println!("\t{}-N --nocache{}\t\tDo not create a cache file: {}WARNING: will delete cache file if exists{}", color::FAINT, color::RESET, color::RED, color::RESET);
-    println!(
-        "\t{}-h, --help{}\t\t\tHelp menu",
-        color::FAINT,
-        color::RESET
-    );
+    println!("\t{}-n, --nocache{}\t\tDo not create a cache file: {}WARNING: will delete cache file if exists{}", color::FAINT, color::RESET, color::RED, color::RESET);
+    println!("\t{}-h, --help{}\t\tHelp menu", color::FAINT, color::RESET);
     println!();
     // Commands list
     println!("Commands:");
+    println!(
+        "\t{}history{}\t\t\tThe historical information of a ticker",
+        color::FAINT,
+        color::RESET
+    );
+}
+
+struct Options {
+    verbose: bool,
+    offline: bool,
+    nocache: bool,
+    quiet: bool,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            verbose: false,
+            offline: false,
+            nocache: false,
+            quiet: false,
+        }
+    }
 }
 
 fn main() {
+    // Gather arguments from terminal
     let args: Vec<String> = std::env::args().collect();
-    let mut args_iter = args.iter().skip(1); // skip name of the program
 
-    // Set current arg to observe
-    let empty_str = String::from("");
-    let arg = args_iter.next().unwrap_or(&empty_str);
+    // Set options
+    let mut options: Options = Options::default();
 
-    if arg == "-h" || arg == "--help" {
-        main_menu(&args[0])
-    } else {
-        main_menu(&args[0])
+    let mut args_iter: std::iter::Peekable<std::slice::Iter<'_, String>> = args.iter().peekable();
+    let mut arg_option: Option<&String> = args_iter.next();
+    while arg_option.is_some() {
+        // Strip short and long option indicatiors
+        let original_arg: String = arg_option.unwrap().to_string();
+        let arg: String = arg_option.unwrap().trim_start_matches("-").to_string();
+
+        match arg.as_str() {
+            // Explore commands
+            arg if !original_arg.starts_with("--") && !original_arg.starts_with("-") => {
+                // Main call <exe name>
+                if arg == args[0] {
+                    if args.len() == 1 {
+                        show_usage(&args[0]);
+                        process::exit(0);
+                    }
+                    // skip main call if more arguments...
+                }
+                // Historical information
+                else if "history".starts_with(arg) {
+                    // TODO - Ticker will consume Info which impl InfoBuilder trait
+                    log(
+                        stdout().lock(),
+                        "Getting historical ticker info.....",
+                        options.verbose,
+                    );
+                    break;
+                }
+                // Last command should be ticker symbol
+                else if arg == &args[args.len() - 1] {
+                    log(
+                        stdout().lock(),
+                        "Getting basic ticker info.....",
+                        options.verbose,
+                    );
+                }
+                // Command not found
+                else {
+                    log(
+                        stdout().lock(),
+                        &format!("{}: invalid command '{}'", &args[0], arg),
+                        true,
+                    );
+                    show_usage(&args[0]);
+                    process::exit(0);
+                }
+            }
+            // Explore options short and long
+            arg if "help".starts_with(arg) => {
+                show_help(&args[0]);
+                process::exit(0);
+            }
+            arg if "nocache".starts_with(arg) => options.nocache = true,
+            arg if "offline".starts_with(arg) => options.offline = true,
+            arg if "quiet".starts_with(arg) => options.quiet = true,
+            arg if "verbose".starts_with(arg) => options.verbose = true,
+            arg if "version".starts_with(arg) || "V" == arg => {
+                log(
+                    stdout().lock(),
+                    &format!(
+                        "{} 0.1.0\nCopyright (C) 2021 Free Software Foundation, Inc.\nLicense GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Samson Ayeni",
+                        &args[0]
+                    ),
+                    true
+                );
+                process::exit(0);
+            }
+            _ => {
+                log(
+                    stdout().lock(),
+                    &format!("{}: invalid option '{}'", &args[0], arg),
+                    true,
+                );
+                show_usage(&args[0]);
+                process::exit(0);
+            }
+        }
+        arg_option = args_iter.next();
     }
 }
