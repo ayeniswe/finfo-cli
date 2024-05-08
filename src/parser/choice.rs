bbe01ac3df40ff1dd4d5d6ec317be8df2d51adf0
Ayeni <n5099f@us.lmco.com>
2024-05-07 19:32:01 -0500

    refactor: added better handling for choices

--- src/parser/choice.rs
+++ src/parser/choice.rs
@@ -1,32 +1,76 @@
-/// The choice states that are made
-pub struct Choice {
-    pub verbose: bool,
-    pub offline: bool,
-    pub nocache: bool,
-    pub quiet: bool,
+pub struct ChoiceName {
+    pub short: char,
+    pub long: String,
 }
 
-impl Default for Choice {
-    fn default() -> Self {
+// Holds the state of a choice that can be on/off
+pub struct ChoiceSwitch {
+    pub enable: bool,
+    pub short: char,
+    pub long: String,
+}
+
+/// All available choices to use within cli
+#[repr(C)]
+pub struct ChoiceGlossary {
+    pub help: ChoiceSwitch,
+    pub nocache: ChoiceSwitch,
+    pub offline: ChoiceSwitch,
+    pub quiet: ChoiceSwitch,
+    pub verbose: ChoiceSwitch,
+    pub version: ChoiceName,
+}
+
+impl ChoiceGlossary {
+    pub fn new() -> Self {
         Self {
-            verbose: false,
-            offline: false,
-            nocache: false,
-            quiet: false,
+            help: ChoiceSwitch {
+                enable: false,
+                short: 'h',
+                long: String::from("help"),
+            },
+            nocache: ChoiceSwitch {
+                enable: false,
+                short: 'n',
+                long: String::from("nocache"),
+            },
+            offline: ChoiceSwitch {
+                enable: false,
+                short: 'o',
+                long: String::from("offline"),
+            },
+            quiet: ChoiceSwitch {
+                enable: false,
+                short: 'q',
+                long: String::from("quiet"),
+            },
+            verbose: ChoiceSwitch {
+                enable: false,
+                short: 'v',
+                long: String::from("verbose"),
+            },
+            version: ChoiceName {
+                short: 'V',
+                long: String::from("version"),
+            },
         }
     }
-}
-
-impl Choice {
     /// Verify if an argument is a valid choice
     /// # Examples
     ///
     /// ```
-    /// assert_eq!(is_choice("--hlpre", "h", "help"), false)
+    /// let choice: Choice = Choice::default();
+    /// assert_eq!(choice.is_choice("--hlpre", "h", "help"), false)
     /// ```
     pub fn is_choice(&self, arg: &str, short: &str, long: &str) -> bool {
         let choice: &str = arg.trim_start_matches("-");
         return arg.starts_with("--") && long.starts_with(choice)
             || arg.starts_with("-") && short == choice;
     }
+}
+
+#[test]
+fn test_is_choice() {
+    let choice: ChoiceGlossary;
+    assert_eq!(choice.is_choice("--hlpre", "h", "help"), false);
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,11 +2,13 @@ mod choice;
 use crate::stream;
 
 pub fn parse_args(args: Vec<String>) {
-    let mut choice: choice::Choice = choice::Choice::default();
+    let mut choice: choice::ChoiceGlossary = choice::ChoiceGlossary::new();
+
+    let cmd_name: &str = &args[0];
 
     let mut args_iter: std::iter::Peekable<std::slice::Iter<'_, String>> = args.iter().peekable();
-
     let mut arg_option: Option<&String> = args_iter.next();
+
     while arg_option.is_some() {
         let arg = arg_option.unwrap();
 
@@ -17,7 +19,7 @@ pub fn parse_args(args: Vec<String>) {
                     // Main call <exe name>
                     arg if arg == args[0] => {
                         if args.len() == 1 {
-                            stream::show_usage(&args[0]);
+                            stream::show_usage(cmd_name);
                             std::process::exit(0);
                         }
                         // skip main call if more arguments...
@@ -28,7 +30,7 @@ pub fn parse_args(args: Vec<String>) {
                         stream::log(
                             std::io::stdout().lock(),
                             "Getting historical ticker info.....",
-                            choice.verbose,
+                            choice.verbose.enable,
                         );
                         break;
                     }
@@ -37,48 +39,41 @@ pub fn parse_args(args: Vec<String>) {
                         stream::log(
                             std::io::stdout().lock(),
                             "Getting basic ticker info.....",
-                            choice.verbose,
+                            choice.verbose.enable,
                         );
                     }
                     // Command not found
                     _ => {
                         stream::log(
                             std::io::stdout().lock(),
-                            &format!("{}: invalid command '{}'", &args[0], arg),
+                            &format!("{}: invalid command '{}'", cmd_name, arg),
                             true,
                         );
-                        stream::show_usage(&args[0]);
+                        stream::show_usage(cmd_name);
                         std::process::exit(0);
                     }
                 }
             }
             // Explore choice short and long
             arg if choice.is_choice(arg, "h", "help") => {
-                stream::show_help(&args[0]);
+                stream::show_help(cmd_name);
                 std::process::exit(0);
             }
-            arg if choice.is_choice(arg, "n", "nocache") => choice.nocache = true,
-            arg if choice.is_choice(arg, "o", "offline") => choice.offline = true,
-            arg if choice.is_choice(arg, "q", "quiet") => choice.quiet = true,
-            arg if choice.is_choice(arg, "v", "verbose") => choice.verbose = true,
+            arg if choice.is_choice(arg, "n", "nocache") => choice.nocache.enable = true,
+            arg if choice.is_choice(arg, "o", "offline") => choice.offline.enable = true,
+            arg if choice.is_choice(arg, "q", "quiet") => choice.quiet.enable = true,
+            arg if choice.is_choice(arg, "v", "verbose") => choice.verbose.enable = true,
             arg if choice.is_choice(arg, "V", "version") => {
-                stream::log(
-                    std::io::stdout().lock(),
-                    &format!(
-                        "{} 0.1.0\nCopyright (C) 2021 Free Software Foundation, Inc.\nLicense GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Samson Ayeni",
-                        &args[0]
-                    ),
-                    true
-                );
+                stream::show_license(cmd_name);
                 std::process::exit(0);
             }
             _ => {
                 stream::log(
                     std::io::stdout().lock(),
-                    &format!("{}: invalid option '{}'", &args[0], arg),
+                    &format!("{}: invalid option '{}'", cmd_name, arg),
                     true,
                 );
-                stream::show_usage(&args[0]);
+                stream::show_usage(cmd_name);
                 std::process::exit(0);
             }
         }
